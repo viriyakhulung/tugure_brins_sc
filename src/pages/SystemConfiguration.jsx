4d0753cda186f2ca5427df7b9c12ade931bfca81
Imam Sanjaya <mam@sibernetik.co.id>
2025-12-23 20:47:48 +0000

    File changes

--- src/pages/SystemConfiguration.jsx
+++ src/pages/SystemConfiguration.jsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Switch } from "@/components/ui/switch";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "@/components/ui/dialog";
 import { Alert, AlertDescription } from "@/components/ui/alert";
